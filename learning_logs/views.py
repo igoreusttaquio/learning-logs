6ba9fb8818f036a0d1112b7a7a4c9f9587557d38
Igor <igoreusttaquio@gmail.com>
2022-01-24 11:46:43 -0300

    View function for the new topic.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
-
+from .forms import TopicForm
 # Create your views here.
 
 # Django passes the request object 
@@ -22,3 +22,21 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added') if topic is not None else None
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics') # View name
+    
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)

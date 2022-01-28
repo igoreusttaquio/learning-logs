134cfa8fb9e8c8a09e8e4bbc41f27718410ea836
Igor <igoreusttaquio@gmail.com>
2022-01-28 10:42:10 -0300

    Registration feature..

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,14 @@
 
 from django.urls import path, include
 
+from . import views
+
 # We set the variable app_name to 'users' so Django can distinguish
 # these URLs from URLs belonging to other apps.
 app_name = 'users'
 urlpatterns = [
     # Inlcude default auth urls.
     path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]

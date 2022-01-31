d26b68e9bb6ff58291c90bc463c6d6cc27cb80db
Igor <igoreusttaquio@gmail.com>
2022-01-31 11:44:08 -0300

    Prevent user modify another user information.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render, redirect
 # before a function definition that Python applies to the function before it
 # runs, to alter how the function code behaves.
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 # Create your views here.
+
+def check_topic_owner(topic, request):
+    if topic.owner != request.user:
+        raise Http404
 
 # Django passes the request object 
 # to this view functions.
@@ -27,6 +32,8 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its asociated entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added') if topic is not None else None
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -41,7 +48,9 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics') # View name
     
 
@@ -53,6 +62,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         # No data submitted; create a blank form.
@@ -75,7 +85,9 @@ def edit_entry(request, entry_id):
     """Editing an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    
+    # Make sure that the entrie belongs to a logged in user.
+    check_topic_owner(topic, request)
+
     if request.method != 'POST':
         # Initial request; pre-fill from with the current entry.
 
